69bb2316fce0fd66769bdd425cbec4fc160b37eb
Michael W <hotdog003@gmail.com>
2010-11-01 12:14:34 -0600

    improved documentation

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,6 +1,11 @@
 /*
- * youtube.js -- whenever someone mentions a video, we'll save the video
- * information
+ * youtube.js -- whenever someone mentions a video, we'll tell the video
+ * information in the chatroom.
+ *
+ * <gcr> hey guys check this video out: http://www.youtube.com/watch?v=dQw4w9WgXcQ
+ * <(lurker)> gcr: ^ that video is 'Rick Astley - Never Gonna Give You Up' (3:33) 27265195 views,18124 likes
+ * <gcr> blast! foiled again!
+ *
  */
 
 var http = require('http'),
@@ -18,29 +23,36 @@ exports.init = function(chat) {
   chat.on('message', function(msg, usr, uid) {
       if (!chat.settled || uid == chat.userId) { return; }
 
+      // look for youtubely URLs
       var yturl = msg.match(/[w.]*youtube\.com\/watch\?v=([a-zA-Z0-9]+)/);
       if (yturl) {
+        // request json from youtube
         // http://gdata.youtube.com/feeds/api/videos/6iVGnaBpBu4?&v=2&alt=jsonc
         http
           .createClient(80, 'gdata.youtube.com')
-          .request('GET', '/feeds/api/videos/'+yturl[1]+'?'+querystring.stringify({
-                v: 2,
-                alt: 'jsonc'
-              }),
+          .request('GET', '/feeds/api/videos/'+yturl[1]+'?'+querystring.stringify({v:2,alt:'jsonc'}),
             {'host':'gdata.youtube.com'})
           .on('response', function(res) {
+            // response could come in more than one packet, so create a buffer
+            // to hold it all
             var body='';
             res.on('data',function(data){body+=data;})
                .on('end', function(end) {
-                 var response = JSON.parse(body.toString().trim());
-                 console.log(require('util').inspect(response));
-                 chat.say(
-                   usr+": ^ that video is '"+response.data.title+"' ("+formatTime(response.data.duration)+") "+
-                     response.data.viewCount+" views"
-                 );
+                 try {
+
+                   // Parse the object we get
+                   var response = JSON.parse(body.toString().trim());
+                   chat.debug(require('util').inspect(response));
+                   chat.say(usr+": ^ that video is '"+
+                       response.data.title+"' ("+formatTime(response.data.duration)+")  with "+
+                       (response.data.viewCount+" views" || "no views") +
+                       (", "+response.data.likeCount+" likes" || "")
+                   );
+
+                 } catch(err) {}
               });
           })
-          .end();
+          .end(); // Send our request
       }
 
     });
