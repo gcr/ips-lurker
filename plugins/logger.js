e8526b29a16753f7838ce87f5b72bfe07d318c39
Michael W <hotdog003@gmail.com>
2010-10-31 23:04:00 -0600

    added dates to logs

--- plugins/logger.js
+++ plugins/logger.js
@@ -1,15 +1,25 @@
 var lockProtect = require('../plugin_glue').lockProtect;
+
+function formatDate() {
+  var d = new Date();
+  return ("00".substr((""+d.getHours()).length)+
+          d.getHours() +
+          ":" +
+          "00".substr((""+d.getMinutes()).length)+
+          d.getMinutes());
+}
+
 exports.init = function(chat) {
   chat.on('message', lockProtect(function(message, username) {
-      console.log("<"+username+"> "+message);
+      console.log(formatDate()+"  <"+username+"> "+message);
     }));
   chat.on('user_noticed', lockProtect(function(username, uid) {
-      console.log("*** just noticed "+username+" with uid "+uid);
+      console.log(formatDate()+"  *** just noticed "+username+" with uid "+uid);
     }));
   chat.on('user_enter', lockProtect(function(username, uid) {
-      console.log("*** "+username+" joined with uid"+uid);
+      console.log(formatDate()+"  *** "+username+" joined with uid"+uid);
     }));
   chat.on('user_exit', lockProtect(function(username) {
-      console.log("*** "+username+" left");
+      console.log(formatDate()+"  *** "+username+" left");
     }));
 };
